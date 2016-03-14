21f51d5c9a7d7a053551f4a32015dedce876e108
jkrysinski1 <justinkrysinski@Justins-MacBook-Pro.local>
2016-03-13 20:56:43 -0500

    Initial

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -10,16 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let weatherUrl = NSURL(string: "api.openweathermap.org/data/2.5/weather?id=4887398&APPID=4847e6bfc1f79cfac8e40ab879df8218")
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
